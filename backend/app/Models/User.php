ba70f29bb553243a92ab11856d0f6501ce802cec
ItsJustmE00 <153960098+ItsJustmE00@users.noreply.github.com>
2025-03-17 02:38:38 +0000

    Create User.php

---
+++ backend/app/Models/User.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'email',
+        'password',
+        'role',
+        'poste',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    /**
+     * Get the pointages for the user.
+     */
+    public function pointages()
+    {
+        return $this->hasMany(Pointage::class);
+    }
+
+    /**
+     * Get the rapports for the user.
+     */
+    public function rapports()
+    {
+        return $this->hasMany(Rapport::class);
+    }
+
+    /**
+     * Check if the user is an admin.
+     */
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
+
+    /**
+     * Get the user's full name.
+     */
+    public function getNomCompletAttribute()
+    {
+        return "{$this->prenom} {$this->nom}";
+    }
+}
+"Ajout du modèle User"
